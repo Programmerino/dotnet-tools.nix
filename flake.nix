761109038f37ea92c6973966dbcda2b1301842ee
Davis Davalos-DeLosh <davis.davalos.delosh@gmail.com>
2023-04-21 20:18:14 -0600

    Adding hawaii

--- flake.nix
+++ flake.nix
@@ -45,13 +45,16 @@
         fable = toolBuilder "fable" "4.0.0-theta-018" "sha256-HueBnXTEZ6xkbWgKqTtHoVIZ/erOa+et2nlqU1/tdO4=" "tools/*/any/fable.dll";
         fantomas = toolBuilder "fantomas" "5.1.3" "sha256-ua0ugBk+saYTyW7DPeH11/ed4AgG+Asv4jzBOrPAuQo=" "tools/*/any/fantomas.dll";
         femto = toolBuilder "Femto" "0.13.0" "sha256-yFZTcO+ht+ENeoW2RQGa6HZ43RloS5pp8F7zuoHIfhU=" "tools/*/any/femto.dll";
+        hawaii = toolBuilder "hawaii" "0.65.0" "sha256-yFZTcO+ht+ENeeW2RQGa6HZ43RloS5pp8F7zuoHIfhU=" "tools/*/any/Hawaii.dll";
       in rec {
         packages.fable = fable;
         packages.fantomas = fantomas;
         packages.femto = femto;
+        packages.hawaii = hawaii;
         apps.fable = flake-utils.lib.mkApp {drv = packages.fable;};
         apps.fantomas = flake-utils.lib.mkApp {drv = packages.fantomas;};
         apps.femto = flake-utils.lib.mkApp {drv = packages.femto;};
+        apps.hawaii = flake-utils.lib.mkApp {drv = packages.hawaii;};
       }
     );
 }
